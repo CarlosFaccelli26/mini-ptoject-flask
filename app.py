814bf434dc644bf47148cf416b9d0e13a0c140f8
CarlosFaccelli26 <carlosfaccelli@gmail.com>
2021-02-12 18:26:18 +0000

    Fixing peek error

--- app.py
+++ app.py
@@ -67,7 +67,7 @@ def login():
         if existing_user:
             # ensure hashed password matcher
             if check_password_hash(
-                existing_user["password"], request.form.get("password")):
+                    existing_user["password"], request.form.get("password")):
                 session["user"] = request.form.get("username").lower()
                 flash("Welcome, {}".format(request.form.get("username")))
                 return redirect(url_for(
